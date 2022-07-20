a99fe25beabf7509b14f71614244267648d47f85
Harshada <harshadajagtap199725@gmail.com>
2022-07-20 20:45:44 +0530

    table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TempSelection from './components/Template Selection/TempSelectionCombine';
+import Pricing from './components/Pricing/Pricing';
 
 function App() {
   return (
     <div className="App">
-     <TempSelection/>
+     {/* <TempSelection/> */}
+     <Pricing/>
     </div>
   );
 }

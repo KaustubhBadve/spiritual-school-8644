b1b984fbe41c9038b38c43fda5c0b839dc96785e
Umesh Yadav <umeshyadav3190@gmail.com>
2022-07-21 21:29:29 +0530

    side bar complete along with sign in login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import TempSelection from './components/Template Selection/TempSelectionCombine';
+// import TempSelection from './components/Template Selection/TempSelectionCombine';
 import MainRoutings from './components/MainRoutings';
+import CombineAllUsersDas from "./components/user Dashboard/CombineAllUserDas"
+import TemplateSelectionCombine from "./components/Template Selection/TempSelectionCombine"
+import Login from './components/Login&Signup/Login';
+import Signup from './components/Login&Signup/SignUp';
 
 
 function App() {
   return (
     <div className="App">
      {/* <TempSelection/> */}
+     {/* <CombineAllUsersDas/> */}
+     {/* <TemplateSelectionCombine/> */}
+     <Login/>
+     <Signup/>
     </div>
   );
 }

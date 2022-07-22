f44f32e229d46245057793f4d5973bd5ba348c15
saikiran003 <kiranarishanpalli@gmail.com>
2022-07-22 15:35:59 +0530

    enterprise page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import "./App.css";
+// import EnterpriseNavbar from "./components/Enterprise/Content/EnterpriseNavbar";
+import EnterpriseContent from "./components/Enterprise/EnterpriseContent";
+import LandingPage from "./components/Landingpage/LandingPage";
+// import CompletePricing from "./components/Pricing/CompletePricing";
+// import Pricing from "./components/Pricing/Pricing";
 
-import './App.css';
-import CompletePricing from './components/Pricing/CompletePricing';
-
-
-import TempSelection from './components/Template Selection/TempSelectionCombine';
+// import TempSelection from "./components/Template Selection/TempSelectionCombine";
 
 function App() {
   return (
     <div className="App">
- 
-     {/* <TempSelection/> */}
+      {/* <TempSelection/> */}
+      {/* <LandingPage /> */}
+      {/* <Pricing/> */}
+      <EnterpriseContent/>
+      
+    </div>
+  );
 }
 
 export default App;
--- src/components/Enterprise/Content/EnterpriseNavbar.jsx
+++ src/components/Enterprise/Content/EnterpriseNavbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../Styles/Homepage.css";
+// import "./Homepage.css";
 
-const NavbarLandingPage = () => {
+const EnterpriseNavbar = () => {
   const [create, setCreate] = useState(false);
   const [learn, setLearn] = useState(false);
   const handleDropdown = () => {
@@ -14,13 +14,16 @@ const NavbarLandingPage = () => {
     setCreate(false);
   };
   return (
-    <div style={{transition:"0.4s"}}>
-      <nav className="navbar navbar-expand-lg fixed-top navbar-light px-md-5" style={{transition:"0.4s"}}>
-        <div className="d-flex pl-md-4 ml-md-5" style={{transition:"0.4s"}}>
+    <div>
+      <nav
+        className="navbar navbar-expand-lg fixed-top navbar-light header-container px-md-5"
+        style={{ backgroundColor: "#24282f" }}
+      >
+        <div className="d-flex pl-md-4 ml-md-5">
           <Link to="/" className="navbar-brand d-flex align-items-center">
             <img
               className="rounded float-left nav-bar__logo"
-              src="https://storage.googleapis.com/lumen5-site-images/L5-logo/L5-logo-header.png"
+              src="https://storage.googleapis.com/lumen5-site-images/L5-logo/logo-white_266x156.png"
               alt="Lumen5 Logo"
             />
           </Link>
@@ -39,11 +42,11 @@ const NavbarLandingPage = () => {
         </button>
         <div
           className="collapse navbar-collapse mr-lg-5 justify-content-end"
-          id="collapsedBarId"
+          id="collapsedBarId" style={{marginLeft:"38%"}}
         >
           <div className="d-flex">
             <div className="q-flex">
-              <div className="">
+              <div className="p-flex">
                 <ul className="navbar-nav">
                   <li className="nav-item dropdown mr-md-2">
                     <Link
@@ -55,11 +58,12 @@ const NavbarLandingPage = () => {
                       aria-expanded="false"
                       onClick={handleDropdown}
                     >
-                      Create
+                      <span style={{ color: "white",fontFamily:"Poppins, sans-serif",letterSpacing:"0.01rem" }}>Create</span>
+
                       <i
                         className="fa fa-angle-down"
                         aria-hidden="true"
-                        style={{ margin: "5px" }}
+                        style={{ margin: "5px", color: "white" }}
                       ></i>
                     </Link>
                     {create && (
@@ -168,12 +172,12 @@ const NavbarLandingPage = () => {
                     )}
                   </li>
 
-                  <li className="nav-item mr-md-2">
+                  <li className="nav-item mr-md-2" style={{ color: "white" }}>
                     <Link
                       className="nav-link text-decoration-none text-dark font-l5 font-l5--regular"
                       to="/pricing/"
                     >
-                      Pricing
+                      <span style={{ color: "white" }}>Pricing</span>
                     </Link>
                   </li>
                   <li className="nav-item mr-md-2">
@@ -181,7 +185,7 @@ const NavbarLandingPage = () => {
                       className="nav-link text-decoration-none text-dark font-l5 font-l5--regular"
                       to="/enterprise/"
                     >
-                      Enterprise
+                      <span style={{ color: "white" }}>Enterprise</span>
                     </Link>
                   </li>
                   <li className="nav-item mr-md-2">
@@ -189,7 +193,7 @@ const NavbarLandingPage = () => {
                       className="nav-link text-decoration-none text-dark font-l5 font-l5--regular"
                       to="/case-studies/"
                     >
-                      Case studies
+                      <span style={{ color: "white" }}>Case studies</span>
                     </Link>
                   </li>
                   <li className="nav-item mr-md-2 dropdown">
@@ -202,11 +206,12 @@ const NavbarLandingPage = () => {
                       aria-expanded="false"
                       onClick={handleLearn}
                     >
-                      Learn
+                      <span style={{ color: "white" }}>Learn</span>
+
                       <i
                         className="fa fa-angle-down"
                         aria-hidden="true"
-                        style={{ margin: "5px" }}
+                        style={{ margin: "5px", color: "white" }}
                       ></i>
                     </Link>
                     {learn && (
@@ -228,6 +233,11 @@ const NavbarLandingPage = () => {
                       to="/auth/login/"
                       type="button"
                       className="lumen5-button2"
+                      style={{
+                        backgroundColor: "#24282f",
+                        color: "white",
+                        border: "1px solid white",
+                      }}
                     >
                       <span>Login</span>
                     </Link>
@@ -251,4 +261,4 @@ const NavbarLandingPage = () => {
   );
 };
 
-export default NavbarLandingPage;
+export default EnterpriseNavbar;
